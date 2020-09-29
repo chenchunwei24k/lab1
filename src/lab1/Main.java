18a24b4d4007c6251e7a8fb4f87ccf13079d54b9
Admin <Admin@ESIR>
2020-09-29 08:47:59 +0800

    2

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is lab 1");
+		System.out.println("s is lab 1");
 	}
 
 }
